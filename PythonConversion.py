3e5e3ee0a45a363e1cbe4c71f8203d5549e48fc8
Tsimpson000 <Tsimpson000@users.noreply.github.com>
2023-12-04 17:15:28 -0800

    Update PythonConversion.py

--- PythonConversion.py
+++ PythonConversion.py
@@ -14,4 +14,28 @@ p4 = Player("Odin", 100)
 
 p5 = Player("Tinsil", 85)
 
-p1.displayPlayer()
+pList = [p1, p2, p3, p4, p5]
+
+print("Players")
+print("1 - Display Players")
+print("2 - Display Players with an Odd letter")
+print("3 - Display Players starting with a letter")
+print("4 - Add Player")
+print("Enter your choice")
+
+userNum = int(input())
+
+print(userNum)
+
+
+if userNum == 1:
+    for player in pList:
+        print(player.displayPlayer())
+elif userNum == 2:
+    for player in pList:
+        if player.number % 2 != 0:
+            print(player.displayPlayer())
+#  elif userNum == 3:
+#  elif userNum == 4:
+else:
+    print("Invalid input")
